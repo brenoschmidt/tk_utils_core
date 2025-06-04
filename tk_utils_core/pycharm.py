eefe47c8178b1f13e1139c06036f0e7ab6b1c4db
Breno Schmidt <breno.schmidt@gmail.com>
2025-06-04 16:20:05 +1000

    fixed pip priority

--- tk_utils_core/pycharm.py
+++ tk_utils_core/pycharm.py
@@ -155,7 +155,10 @@ class TKPaths:
         venv_bin = venv_root.joinpath('bin' if POSIX else 'Scripts')
 
         # possible executable names (in order or priority)
-        _pips = ['pip', 'pip3', 'pip.exe', 'pip3.exe']
+        if POSIX:
+            _pips = ['pip', 'pip3']
+        else:
+            _pips = ['pip.exe', 'pip3.exe', 'pip', 'pip3']
         pips = [venv_bin.joinpath(x) for x in _pips]
         venv = {
                 'root': venv_root,
