1edd1badf4d77a5f10d4347af59626b458fa41a4
Breno Schmidt <breno.schmidt@gmail.com>
2025-06-06 11:51:42 +1000

    added branch parm to git_url

--- tk_utils_core/core/webtools/github.py
+++ tk_utils_core/core/webtools/github.py
@@ -6,8 +6,11 @@ Github utils
 from __future__ import annotations
 
 
-def git_url(user: str, repo: str):
-    return f"https://github.com/{user}/{repo}.git"
+def git_url(user: str, repo: str, branch: str | None = None):
+    tgt = f"https://github.com/{user}/{repo}.git"
+    if branch:
+        tgt = f"{tgt}@{branch}"
+    return tgt
 
 def cnts_url(user: str, repo: str, branch: str):
     return f"https://raw.githubusercontent.com/{user}/{repo}/{branch}"
