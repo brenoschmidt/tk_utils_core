af18bc86a0f30c9326fda73996de49b88448f6c5
Breno Schmidt <breno.schmidt@gmail.com>
2025-07-02 13:14:36 +1000

    removed empty lines

--- tk_utils_core/codeparser.py
+++ tk_utils_core/codeparser.py
@@ -3,11 +3,12 @@ Code parsers
 """
 from __future__ import annotations
 
-
-from tk_utils_core.core.codeparser._codeparser import (
+from tk_utils_core.core.codeparser._parso import (
         ParsedFunc,
+        ModuleDefs,
         )
 
 __all__ = [
         'ParsedFunc',
+        'ModuleDefs',
         ]
