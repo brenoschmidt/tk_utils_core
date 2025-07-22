f44706c58a4bbcd8630c0a40ba39b56448a8607c
Breno Schmidt <breno.schmidt@gmail.com>
2025-07-23 08:31:48 +1000

    added constants.py module

---
+++ tk_utils_core/constants.py
@@ -0,0 +1,15 @@
+""" 
+Constants
+
+         
+"""
+from __future__ import annotations
+
+from tk_utils_core.core.constants import (
+        POSIX,
+        )
+
+
+__all__ = [
+        'POSIX',
+        ]
